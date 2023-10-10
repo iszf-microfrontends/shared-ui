6e211db5286e70db206d9a7f23fbe5a22dce7de6
Sviatoslav <sviatoslavnazin@gmail.com>
2023-10-10 21:32:17 +0800

    bump deps & remove useless eslint packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   extends: ['@iszf-microfrontends/eslint-config'],
-  ignorePatterns: ['rollup.config.js'],
+  ignorePatterns: ['dist', 'coverage', '.eslintrc.cjs', 'rollup.config.js'],
   rules: {
     'import/export': 'off',
   },
